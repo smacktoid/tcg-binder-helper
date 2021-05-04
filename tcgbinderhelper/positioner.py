01695739abd8a31560bc4a31dd2bd9e554ca3c26
Scott Mackie <scott.mackie@verint.com>
2021-05-04 01:20:30 +0100

    More positional tests

--- tcgbinderhelper/positioner.py
+++ tcgbinderhelper/positioner.py
@@ -1,12 +1,30 @@
+import math
+from enum import Enum
+
+
+class Side(Enum):
+    FRONT = 1
+    BACK = 2
+
+
 class CardPosition(object):
-    def __init__(self, page, slot):
+    def __init__(self, page, slot, side: Side):
         self.page = page
         self.slot = slot
+        self.side = side
 
 
-
-
-def find(card_number)->CardPosition:
+def find_position(card_number) -> CardPosition:
     print(f'Finding position for card {card_number}')
     slot = card_number % 18
-    return CardPosition(0, slot)
+    if slot == 0:
+        slot = 18
+
+    if slot > 9:
+        slot = slot - 9
+        side = Side.BACK
+    else:
+        side = Side.FRONT
+
+    page = math.ceil(card_number / 18)
+    return CardPosition(page, slot, side)
--- tests/test_positioner.py
+++ tests/test_positioner.py
@@ -1,12 +1,27 @@
 import unittest
-from tcgbinderhelper import positioner
+from tcgbinderhelper.positioner import find_position, Side
 
 
 class TestPositioner(unittest.TestCase):
 
-    def test_first_card_is_in_first_slot_on_page_one(self):
-        position = positioner.find(1)
+    def test_first_card_is_in_first_slot_on_front_ofpage_one(self):
+        position = find_position(1)
         self.assertEqual(1, position.slot)
+        self.assertEqual(1, position.page)
+        self.assertEqual(Side.FRONT, position.side)
+
+    def test_eighteenth_card_is_on_ninth_slot_of_back_of_page_one(self):
+        position = find_position(18)
+        self.assertEqual(9, position.slot)
+        self.assertEqual(1, position.page)
+        self.assertEqual(Side.BACK, position.side)
+
+    def test_page_calculated_correctly_after_first_page(self):
+        position = find_position(19)
+        self.assertEqual(2, position.page)
+        self.assertEqual(1, position.slot)
+        self.assertEqual(Side.FRONT, position.side)
+
 
 if __name__ == '__main__':
     unittest.main()
